7d8f38e51aa08fe9aa6b54b4f777bbcccfa1d09f
Kevin Ummel <ummel@sas.upenn.edu>
2021-06-24 11:41:03 -0600

    Enable parallel processing on UNIX systems. Allow disabling of 'maxcats'.

--- R/train.R
+++ R/train.R
@@ -9,8 +9,8 @@
 #' @param ignore Character vector. Alternative way to specify predictor variables. Can include regular expressions. If non-NULL, all variables other than those in \code{y}, \code{weight}, and \code{ignore} are used. Only one of \code{x} and \code{ignore} can be non-NULL.
 #' @param weight Character vector. Name of the observation weights column. If NULL (default), uniform weights are assumed.
 #' @param mc Logical. Should multicore processing be used? Implementation uses \code{\link[parallel]{mclapply}}, so \code{mc = TRUE} will fail on Windows.
+#' @param maxcats Positive integer or NULL. Maximum number of levels allowed in an unordered factor predictor variable when the response (fusion) variable is also an unordered factor. Prevents excessive \code{\link[rpart]{rpart}} computation time. A K-means clustering strategy is used to cluster the predictor to no more than \code{maxcats} levels.
 #' @param lasso Numeric (0-1) or NULL. Controls extent of (fast) predictor variable pre-screening via LASSO regression. If NULL (default), no pre-screening is performed. See Details.
-#' @param maxcats Positive integer. Maximum number of levels allowed in an unordered factor predictor variable when the response (fusion) variable is also an unordered factor. Prevents excessive \code{\link[rpart]{rpart}} computation time. A K-means clustering strategy is used to cluster the predictor to no more than \code{maxcats} levels.
 #' @param ... Optional arguments passed to \code{\link[rpart]{rpart}} to control tree-building. By default \code{cp = 0}, \code{xval = 0}, \code{minbucket = 50} (\code{minbucket = 10} for discrete models), and all other arguments are left at default values.
 #'
 #' @details When \code{lasso} is non-NULL, predictor variables are "pre-screened" using LASSO regression via \code{\link[rpart]{glmnet}} prior to fitting a \code{\link[rpart]{rpart}} model. Predictors with a LASSO coefficient of zero are excluded from consideration. This can speed up tree-fitting considerably when \code{data} is large. Lower values of \code{lasso} are more aggressive at excluding predictors; the LASSO \emph{lambda} is chosen such that the deviance explained is at least \code{lasso}-% of the maximum. To ensure the LASSO step is fast, pre-screening is only used for numeric and ordered factor response variables. No predictor pre-screening occurs when the response is an unordered factor.
@@ -53,15 +53,15 @@ train <- function(data,
                   ignore = NULL,
                   weight = NULL,
                   mc = FALSE,
+                  maxcats = NULL,
                   lasso = NULL,
-                  maxcats = 10,
                   ...) {
 
   stopifnot(exprs = {
     is.data.frame(data)
     !missing(y)
-    maxcats > 1 & maxcats %% 1 == 0
     is.logical(mc)
+    is.null(maxcats) | (maxcats > 1 & maxcats %% 1 == 0)
     is.null(lasso) | (lasso >= 0 & lasso <= 1)
   })
 
@@ -69,7 +69,7 @@ train <- function(data,
   if (!is.null(x) & !is.null(ignore)) stop("Only one of 'x' or 'ignore' may be non-NULL")
 
   # Set default rpart() arguments and modify with user-supplied arguments, if necessary
-  rpart.args <- list(cp = 0, xval = 0, minbucket = 50)  # Default rpart() arguments
+  rpart.args <- list(cp = 0, xval = 0, minbucket = 50, maxcompete = 0)  # Default rpart() arguments
   rpart.default <- TRUE
   if (!missing(...)) {
     rpart.default <- FALSE
@@ -81,13 +81,9 @@ train <- function(data,
     rpart.args[names(usr.args)] <- usr.args
   }
 
-  # Set function to use for parallel processing, if mc = TRUE
-  if (mc) {
-    mcFun <- parallel::mclapply
-    options(mc.cores = max(1L, parallel::detectCores() - 1L))
-  } else {
-    mcFun <- lapply
-  }
+  # Number of cores to use in parallel
+  # If NULL, defaults to lapply() behavior via pblapply()
+  mc.cores <- if (mc) max(1L, parallel::detectCores() - 1L) else NULL
 
   #-----
 
@@ -173,56 +169,52 @@ train <- function(data,
   cat("Determining order of fusion variables...\n")
 
   # Fit full models, including yvars as predictors
-  # Excludes variables identified in 'lasso.ignore'
   # Only 'variable.importance' slot is returned
-  full.varimp <- mcFun(yvars, function(y) {
+  full.varimp <- pbapply::pblapply(X = yvars, FUN = function(y) {
     m <- fitRpart(y = y,
                   x = c(xvars, setdiff(yvars, y)),
                   w = w,
                   data = data,
-                  args = if (rpart.default & !y %in% ycont) replace(rpart.args, 3, 10) else rpart.args,
+                  maxcats = maxcats,
+                  args = if (rpart.default & !y %in% ycont) replace(rpart.args, 3, 10) else rpart.args, # Sets default minbucket = 10 in the discrete case
                   lasso.threshold = lasso)
+    gc()  # Perhaps useful when run in parallel
     m$variable.importance
-  }) %>%
+  }, cl = mc.cores) %>%
     setNames(yvars)
 
   # Find preferred order of yvars
   yord <- fusionOrder(varimp = full.varimp)
+
+  # Cleanup
+  rm(full.varimp)
 
   #----------------------------------
   #----------------------------------
 
   # FIT MODELS
 
-  # Output list for rpart() models
-  models <- vector(mode = "list", length = length(yord))
-  names(models) <- yord
-
-  # Assemble dummy matrix for 'xvars'
-  ranks <- lapply(xvars, matFun, data = data)
+  # Assemble dummy matrix for 'xvars' and 'yvars'
+  ranks <- lapply(c(xvars, yord), matFun, data = data)
+  ranks.var <- unlist(mapply(rep, x = c(xvars, yord), each = sapply(ranks, ncol)), use.names = FALSE)  # Variable associated with each column of 'ranks' matrix
   ranks <- do.call(cbind, ranks)
 
   # Placeholder list for 'ycor' with slots for continuous and ordered factor fusion variables (but not unordered factors)
   ycor.vars <- names(which(sapply(yclass, function(x) x[1] != "factor")))
-  ycor <- vector(mode = "list", length = length(ycor.vars))
-  names(ycor) <- ycor.vars
 
   #-----
 
-  cat("Training fusion models...\n")
+  cat("Building fusion models...\n")
 
-  # Progress bar printed to console
-  pb <- txtProgressBar(min = 0, max = length(yord), style = 3)
-
-  # Sequentially fit models
-  for (i in 1:length(yord)) {
+  # Function to fit rpart() model and calculate rank correlations for i-th response variable in 'yord'
+  fitModel <- function(i) {
 
     y <- yord[i]
 
     # Is 'y' continuous?
     cont <- y %in% ycont
 
-    # Prior response variables
+    # Prior response variables in the fusion sequence
     yprior <- if (i == 1) NULL else yord[1:(i - 1)]
 
     #--------
@@ -249,7 +241,7 @@ train <- function(data,
       nodes <- sort(unique(m$where))
 
       # Placeholder matrix for the smoothed quantile values
-      Qn <- 500  # Should be function control argument
+      Qn <- 500  # Ideally, would be a train() control argument
       m$Q <- matrix(0L, nrow = Qn, ncol = length(nodes))
       colnames(m$Q) <- nodes
 
@@ -271,42 +263,44 @@ train <- function(data,
 
     #--------
 
-    # Save the model...
-    models[[i]] <- slimRpart(m)
-
-    # Create 'ranks' matrix addition for 'y'
-    yrank <- matFun(y, data)
-
     # Calculate (rank) correlation between 'y' and existing variables in 'ranks' (if necessary; only continuous and ordered factors)
-    if (y %in% names(ycor)) {
+    # NOTE: Correlation calculation restricted to observations where 'y' is non-zero
+    if (y %in% ycor.vars) {
       ind <- which(as.numeric(data[[y]]) > 0)
-      p <- suppressWarnings(cor(ranks[ind, ], yrank[ind, ]))[, 1]  # Will silently return NA if no variance in a particular column
-      p <- p[!is.na(p)]
-      ycor[[y]] <- cleanNumeric(p, tol = 0.001)
+      j <- which(ranks.var == y)  # Column in 'ranks' associated with 'y'
+      p <- suppressWarnings(cor(ranks[ind, 1:(j - 1)], ranks[ind, j]))[, 1]  # Will silently return NA if no variance in a particular column
+      p <- cleanNumeric(p[!is.na(p)], tol = 0.001)
+    } else {
+      p <- NULL
     }
 
-    # Update the 'ranks' matrix
-    # This is memory-inefficient for large datasets; should pre-allocate 'ranks', if possible
-    ranks <- cbind(ranks, yrank)
+    # Cleanup
+    gc()  # Perhaps useful when run in parallel
 
-    # Update progress bar
-    setTxtProgressBar(pb, i)
+    return(list(m = slimRpart(m), p = p))
 
   }
 
-  # Close progress bar
-  close(pb)
+  #-----
+
+  # Call fitModel() for each 'yord', possibly in parallel
+  mout <- pbapply::pblapply(
+    X = 1:length(yord),
+    FUN = fitModel,
+    cl = mc.cores)
+  names(mout) <- yord
 
   #-----
 
   # Assemble final result
-  result <- list(models = models,
-                 xclass = xclass,
-                 xlevels = xlevels,
-                 yclass = yclass,
-                 ylevels = ylevels,
-                 yinner = yinner,
-                 ycor = ycor
+  result <- list(
+    models = map(mout, "m"),
+    xclass = xclass,
+    xlevels = xlevels,
+    yclass = yclass,
+    ylevels = ylevels,
+    yinner = yinner,
+    ycor = compact(map(mout, "p"))
   )
 
   return(result)
