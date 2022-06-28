b8bb8bae5ae9ddee963f97dd5dbe6bfe4364e4dd
Kevin Ummel <ummel@sas.upenn.edu>
2022-06-27 20:49:32 -0600

    Moved CART helpers to separate utils_cart.

--- R/utils_cart.R
+++ R/utils_cart.R
@@ -686,3 +686,80 @@ detectCategoricalDependence <- function(data, fvars, exclude, suffix = "_BINARY_
   return(out)
 
 }
+
+#-----------------------------------------
+#-----------------------------------------
+
+# Remove elements of rpart() model object to reduce size while still allowing prediction
+slimRpart <- function(m) {
+  m$call <- NULL
+  m$numresp <- NULL
+  m$parms <- NULL
+  m$functions <- NULL
+  m$ordered <- NULL
+  m$y <- NULL
+  #attr(m, "xlevels") <- NULL
+  if ("splits" %in% names(m)) m$splits[, c('improve', 'adj')] <- 0
+  if ("frame" %in% names(m)) m$frame[, c('n', 'wt', 'dev', 'complexity')] <- 0
+  if ("terms" %in% names(m)) attributes(m$terms)['.Environment'] <- NULL
+  return(m)
+}
+
+#-----------------------------------------
+#-----------------------------------------
+
+# Remove elements of biglm() model object to reduce size while still allowing prediction
+# slimBiglm <- function(m) {
+#   m$call <- NULL
+#   m$assign <- NULL
+#   m$df.resid <- NULL
+#   m$weights <- NULL
+#   m$n <- NULL
+#   m$names <- NULL
+#   return(m)
+# }
+
+#-----------------------------------------
+#-----------------------------------------
+
+# Validate input variable vector against a given set of names
+# 'x' can include regular expressions
+validNames <- function(x, nms, exclude = FALSE) {
+  rgx <- setdiff(x, nms)
+  v <- c(intersect(x, nms), unlist(lapply(rgx, function(x) grep(x, nms, value = TRUE))))
+  if (exclude) v <- setdiff(nms, v)
+  out <- v[na.omit(match(nms, v))]
+  return(out)
+}
+
+#-----------------------------------------
+#-----------------------------------------
+
+# Return rpart() model prediction node for each observation in 'newdata'
+# Based on: https://github.com/cran/rpart.plot/blob/master/R/rpart.predict.R
+predictNode <- function(object, newdata) {
+
+  where <-
+    if (missing(newdata)) {
+      object$where
+    } else {
+      if(is.null(attr(newdata, "terms"))) {
+        Terms <- delete.response(object$terms)
+        newdata <- model.frame(Terms, newdata, na.action = na.pass,
+                               xlev = attr(object, "xlevels"))
+        if(!is.null(cl <- attr(Terms, "dataClasses")))
+          .checkMFClasses(cl, newdata, TRUE)
+      }
+      newdata <- getFromNamespace("rpart.matrix", ns="rpart")(newdata)
+      getFromNamespace("pred.rpart", ns="rpart")(object, newdata)
+    }
+
+  # nn <- as.numeric(rownames(object$frame)[where])
+  #
+  # # Index assigning each row in 'pred' to a node number
+  # node <- match(nn, as.integer(row.names(object$frame)))
+
+  #return(node)
+  return(where)
+
+}
