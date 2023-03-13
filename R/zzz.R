1bb0bd39416653e7212ba40efa0b57ef1f47410c
Kevin Ummel <ummel@sas.upenn.edu>
2023-03-13 17:32:58 -0600

    Moved package imports to zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,12 @@
-.onAttach <- function (libname, pkgname) {
+# Specify package imports
+#' @import dplyr
+#' @rawNamespace import(stats, except = c(filter, lag))
+#' @rawNamespace import(data.table, except = c(first, last, between))
+NULL
+
+#-----
+
+.onLoad <- function (libname, pkgname) {
 
   packageStartupMessage("fusionModel v", utils::packageVersion("fusionModel"), " | https://github.com/ummel/fusionModel")
 
