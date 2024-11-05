ef18572b7a136c5ebe76941e9d60d7400f90fc22
ummel <ummel@berkeley.edu>
2024-11-05 13:54:42 -0700

    Allow read_fsd() to read generic .fst file.

--- R/read_fsd.R
+++ R/read_fsd.R
@@ -1,11 +1,12 @@
 #' Read fusion output from disk
 #'
-#' @description Read fusion output that was written directly to disk via \code{\link{fuse}}.
-#' @param fsd Character. File path ending in \code{.fsd} as produced by call to \code{\link{fuse}}.
-#' @param columns Character. Column names to read. The default is to read all columns.
-#' @param cores Integer. Number of cores used by \code{\link[fst]{read_fst}}.
-#' @details As of version 2.3.0, this is simply a convenient wrapper around \code{\link[fst]{read_fst}}, since fusion output data files (.fsd) are actually native \code{\link[fst]{fst}} files under the hood.
-#' @return A \code{\link[data.table]{data.table}} with integer column "M" indicating the implicate assignment of each observation. Note that the ordering of recipient observations is consistent within implicates, so do not change the row order if using with \code{\link{analyze}} or \code{\link{validate}}.
+#' @description Efficiently read fusion output that was written to disk, optionally returning a subset of rows and/or columns. Since a \code{.fsd} file is simply a \code{\link[fst]{fst}} file under the hood, this function also works on any \code{.fst} file.
+#' @param path Character. Path to a \code{.fsd} (or \code{.fst}) file, typically produced by \code{\link{fuse}}.
+#' @param columns Character. Column names to read. The default is to return all columns.
+#' @param df Data frame. Data frame used to identify a subset of rows to return. Default is to return all rows.
+#' @param cores Integer. Number of cores used by \code{\link[fst]{fst}}.
+#' @details If \code{df} is provided and the file size on disk is less than 250 MB, then a full read and inner \code{\link[collapse]{join}} is performed. For larger files, a manual read of the required rows is performed, using \code{\link[collapse]{fmatch}} for the matching operation.
+#' @return A \code{\link[data.table]{data.table}}; keys are preserved if present in the on-disk data. When \code{path} points to a \code{.fsd} file, it includes an integer column "M" indicating the implicate assignment of each observation (unless explicitly ignored by \code{columns}).
 #' @examples
 #' # Build a fusion model using RECS microdata
 #' # Note that "fusion_model.fsn" will be written to working directory
@@ -17,7 +18,7 @@
 #' # Write fusion output directly to disk
 #' # Note that "results.fsd" will be written to working directory
 #' recipient <- recs[predictor.vars]
-#' sim <- fuse(data = recipient, fsn = fsn.path, M = 5, csv = "results.fsd")
+#' sim <- fuse(data = recipient, fsn = fsn.path, M = 5, fsd = "results.fsd")
 #'
 #' # Read the fusion output saved to disk
 #' sim <- read_fsd(sim)
@@ -25,21 +26,47 @@
 #'
 #' @export
 
-read_fsd <- function(fsd,
+read_fsd <- function(path,
                      columns = NULL,
+                     df = NULL,
                      cores = max(1, parallel::detectCores(logical = FALSE) - 1)) {
 
   stopifnot({
-    endsWith(fsd, ".fsd")
-    file.exists(fsd)
+    endsWith(path, ".fsd") | endsWith(path, ".fst")
+    file.exists(path)
+    is.null(columns) | is.character(columns)
+    is.null(df) | is.data.frame(df)
+    cores > 0 & cores %% 1 == 0
   })
 
+  require(collapse, quietly = TRUE)
   n <- fst::threads_fst()
   fst::threads_fst(nr_of_threads = cores)
-  d <- fst::read_fst(path = fsd,
-                     columns = columns,
-                     as.data.table = TRUE)
-  fst::threads_fst(nr_of_threads = n)  # Reset number of threads
+
+  if (is.null(df)) {
+    d <- fst::read_fst(path, columns = columns, as.data.table = TRUE)
+  } else {
+    meta <- fst::metadata_fst(path)
+    stopifnot(all(names(df) %in% meta$columnNames))
+    df <- unique(qDT(df))
+    # If the file size is less than 250 MB, simply read the full data and subset via collapse::join()
+    if (file.size(path) / 1e6 < 250) {
+      d <- fst::read_fst(path, columns = columns, as.data.table = TRUE)
+      d <- collapse::join(d, df, how = "inner", verbose = FALSE)
+    } else {
+      if (is.null(columns)) columns <- meta$columnNames else stopifnot(all(columns %in% meta$columnNames))
+      d <- fst::fst(path)
+      m <- qDT(d[names(df)])
+      i <- m %iin% df  # Collapse equivalent of which(x %in% table) using fmatch()
+      d <- qDT(d[i, setdiff(columns, names(m))])
+      d <- cbind(d, m[i, ])
+      setcolorder(d, columns)
+      setkeyv(d, cols = meta$keys)
+    }
+  }
+
+  # Reset number of threads
+  fst::threads_fst(nr_of_threads = n)
 
   return(d)
 
